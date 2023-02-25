a95e0fdf90cc9e8120cb2d56499889cc5e0f2e34
MohitKashyapUK <92683467+MohitKashyapUK@users.noreply.github.com>
2023-02-25 18:32:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route('/<string:n>')
-def index(n):
+@app.route('/<string:n>/<string:g>')
+def index(n,g):
     yt = YouTube(f"http://youtube.com/watch?v={n}")
-    video = streams = yt.streams.get_highest_resolution()
+    video = streams = yt.streams.get_by_resolution(g)
     download = video.download()
     return send_file(download)
 
