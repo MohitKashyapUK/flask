323271e7e3140163237bdb9ab4654b43fe7c42b3
MohitKashyapUK <92683467+MohitKashyapUK@users.noreply.github.com>
2023-02-25 16:04:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 import os
-import requests
 
 app = Flask(__name__)
 
@@ -11,12 +10,7 @@ def index():
 
 @app.route('/about')
 def about():
-    name = requests.get('https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png')
-    try:
-        return send_from_directory(name, path='/', as_attachment=True)
-    except FileNotFoundError:
-        abort(404)
-    return name
+    return 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
