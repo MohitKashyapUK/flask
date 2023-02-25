3072318e17231419d6986a91b4e997e0ce3dce5b
MohitKashyapUK <92683467+MohitKashyapUK@users.noreply.github.com>
2023-02-25 15:43:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def index():
 @app.route('/about')
 def about():
     name = requests.get('https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png')
+    try:
+        return send_from_directory(name, path='/', as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
     return name
 
 if __name__ == '__main__':
