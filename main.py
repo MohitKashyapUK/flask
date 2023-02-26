92b7aeec5e5f1296d1e51d8721d5269a98fed926
MohitKashyapUK <92683467+MohitKashyapUK@users.noreply.github.com>
2023-02-26 18:24:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,35 @@ from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route("/yt/streams/<string:n>")
+@app.route("/")
+def index():
+  return "<b style='font-size: 50px;'>This is YouTube Video Download Bot<br />goto /yt</b>"
+
+@app.route("/yt/")
+def yt():
+  o = ["/yt/streams/video_id","/yt/video/download/video_id/resolution"]
+  return json.dumps(o)
+
+@app.route("/yt/video/size/<string:n>/")
+def size(n):
+  yt = YouTube(f"http://youtube.com/watch?v={n}")
+  x = ""
+  for i in streams:
+    x += str(i).split()[3][5:-1]
+    x += " "
+  c = x.split()
+  c.pop()
+  o = []
+  s = {}
+  for i in c:
+    if i not in o:
+      o.append(i)
+  for i in o:
+    k = yt.streams.get_by_resolution(i).filesize_mb
+    s[i] = k
+  return json.dumps(s)
+
+@app.route("/yt/streams/<string:n>/")
 def streams(n):
   yt = YouTube(f"http://youtube.com/watch?v={n}")
   streams = yt.streams.filter(file_extension='mp4').order_by("resolution")
@@ -16,14 +44,13 @@ def streams(n):
   c = x.split()
   c.pop()
   o = []
-  count = 0
   for i in c:
     if i not in o:
       o.append(i)
   g = json.dumps(o)
   return g
 
-@app.route('/yt/video/<string:n>/<string:g>')
+@app.route('/yt/video/download/<string:n>/<string:g>/')
 def video(n,g):
     yt = YouTube(f"http://youtube.com/watch?v={n}")
     video = streams = yt.streams.get_by_resolution(g)
