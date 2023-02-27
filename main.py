d0a438b697fc225cdae2f46e230754613512391c
MohitKashyapUK <92683467+MohitKashyapUK@users.noreply.github.com>
2023-02-27 15:11:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,42 +14,25 @@ def yt():
   o = ["/yt/streams/video_id","/yt/video/download/video_id/resolution"]
   return json.dumps(o)
 
-@app.route("/yt/video/size/<string:n>/")
+@app.route("/yt/video/info/<string:n>/")
 def ytvideosize(n):
   yt = YouTube(f"http://youtube.com/watch?v={n}")
   streams = yt.streams.filter(file_extension='mp4').order_by("resolution")
   final = ""
   for i in streams:
-    stri = str(i).split()
-    itag = stri[1][6:-1]
-    res = stri[3][5:-1]
+    itag = i.itag
+    res = i.resolution
     size = i.filesize_mb
-    final += f"Video res: {res}, itag: {itag}, Video size: {size}mb <br />"
-  return json.dumps(final)
-
-@app.route("/yt/video/resolution/<string:n>/")
-def ytvideoresolution(n):
-  yt = YouTube(f"http://youtube.com/watch?v={n}")
-  streams = yt.streams.filter(file_extension='mp4').order_by("resolution")
-  x = ""
-  for i in streams:
-    x += str(i).split()[3][5:-1]
-    x += " "
-  c = x.split()
-  c.pop()
-  o = []
-  for i in c:
-    if i not in o:
-      o.append(i)
-  g = json.dumps(o)
-  return g
+    have_audio = i.include_audio_track
+    final += f"Video res: {res}, itag: {itag}, Video size: {size}mb, Have audio: {have_audio}<br />"
+  return json.dumps(final)[1:-1]
 
 @app.route('/yt/video/download/<string:videoid>/<int:itag>/')
 def ytvideodownload(videoid,itag):
     yt = YouTube(f"http://youtube.com/watch?v={videoid}")
-    video = streams = yt.streams.get_by_itag(itag)
-    download = video.download()
-    return send_file(download)
+    video = yt.streams.get_by_itag(itag)
+    url = json.dumps(f"<a style='font-size:50%;' href='{video.url}' />")
+    return url
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
