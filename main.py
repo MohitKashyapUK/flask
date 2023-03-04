d3c465312a8d1aa57a267bf95cc82655235c1779
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-05 01:56:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ url = f"https://api.telegram.org/bot{token}/sendMessage"
 @app.route("/webhook", methods = ["GET", "POST"])
 def index():
   request_args = request.get_json()
-  message = request_args["result"][0]["message"]["text"]
-  chat_id = request_args["result"][0]["message"]["chat"]["id"]
-  message_id = request_args["result"][0]["message"]["message_id"]
+  message = request_args.get("result")[0]["message"]["text"]
+  chat_id = request_args.get("result")[0]["message"]["chat"]["id"]
+  message_id = request_args.get("result")[0]["message"]["message_id"]
   requests.post(url,data={"chat_id": chat_id, "text": f"message: {message},\nchat_id: {chat_id},\nmessage_id: {message_id}"})
   return json.dumps({"ok":True})
 
