fbe6b8c74e3967b6578a7f7c7bf7f2afaa691932
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-05 16:23:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 #url = f"https://api.telegram.org/bot{token}/sendMessage"
 
 @app.route("/webhook", methods = ["GET", "POST"])
-def index():
-  request_args = {
+def webhook():
+  """request_args = {
     "args":type(request.args),
     "argsdata":request.args,
     "data":type(request.data),
@@ -18,12 +18,13 @@ def index():
     "jsondata":request.json,
     "get_json":type(request.get_json()),
     "get_jsondata":request.get_json()
-  }
-  """message = request_args["result"][0]["message"]["text"]
-  chat_id = request_args["result"][0]["message"]["chat"]["id"]
-  message_id = request_args["result"][0]["message"]["message_id"]
-  requests.post(url,data={"chat_id": chat_id, "text": f"message: {message},\nchat_id: {chat_id},\nmessage_id: {message_id}"})"""
-  print(request_args)
+  }"""
+  data = request.get_json()
+  message = data["message"]["text"]
+  chat_id = data["message"]["chat"]["id"]
+  message_id = data["message"]["message_id"]
+  requests.post(url,data={"chat_id": chat_id, "text": f"message: {message},\nchat_id: {chat_id},\nmessage_id: {message_id}"})
+  #print(request_args)
   return {'ok':True}
 if __name__ == "__main__":
   app.run(debug=True)
