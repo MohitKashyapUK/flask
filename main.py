9343ffc84bc2ae434264469e280b5732218d820b
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-05 23:37:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ url = f"https://api.telegram.org/bot{token}/sendMessage"
 @app.route("/webhook", methods = ["GET", "POST"])
 def webhook():
   data = request.get_json()
-  message = data["message"]["text"]
+  print(data)
+  """message = data["message"]["text"]
   chat_id = data["message"]["chat"]["id"]
   message_id = data["message"]["message_id"]
   text_list = ['ok','hello!','namaste!','sasriakaal','we are working on this bot!']
@@ -19,8 +20,8 @@ def webhook():
     return random.randint(0,4)
   if message == '/start':
     requests.post(url,data={"chat_id": chat_id, 'text': text_list[getrandomtext()]})
-  else:
-    requests.post(url,data={"chat_id": chat_id, 'text': 'Hey!'})
+  else:"""
+  requests.post(url,data={"chat_id": chat_id, 'text': 'Hey!'})
   return {'ok':True}
 if __name__ == "__main__":
   app.run(debug=True)
