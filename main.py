40ba572ebf2fc95af680635fbcfa76b951eaf0d8
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-06 16:12:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 token = os.environ["TOKEN"]
 url = f"https://api.telegram.org/bot{token}/sendMessage"
-base_url = f'http://0.0.0.0:8000/bot{token}/sendMessage'
+#base_url = f'{request.host_url}/bot{token}/sendMessage'
+host_url = request.host_url
 
 @app.route("/webhook", methods = ["GET", "POST"])
 def webhook():
-  data = request.get_json()
+  '''data = request.get_json()
   message_id = data["message"]["message_id"]
   chat_id = data["message"]["chat"]["id"]
   message = data["message"].get("text")
@@ -28,13 +29,12 @@ def webhook():
     requests.post(url,data={"chat_id":chat_id,"text":"Audio!"})
   else:
     print(data)
-    requests.post(url,data={"chat_id":chat_id,"text":"Other!"})
+    requests.post(url,data={"chat_id":chat_id,"text":"Other!"})'''
   '''
   file_res = requests.get(f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}")
   file_path = file_res["result"]["file_path"]
   file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
   '''
-  return {'ok':True}
+  return host_url
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 8000))
-  app.run(host='0.0.0.0', port=port)
+  app.run()
