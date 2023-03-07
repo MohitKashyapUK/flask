fcd44fdb3cce64e1b95ac4890f76d88f331ba6e7
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-07 14:26:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import os
 import json
 app = Flask(__name__)
 
-'''token = os.environ["TOKEN"]
+token = os.environ["TOKEN"]
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 #base_url = f'{request.host_url}/bot{token}/sendMessage'
-'''
-base_url = 'http://0.0.0.0:6457/bot{token}/sendMessage'
+
+#base_url = 'http://0.0.0.0:6457/bot{token}/sendMessage'
 
 @app.route("/webhook", methods = ["GET", "POST"])
 def webhook():
-  '''data = request.get_json()
+  data = request.get_json()
   message_id = data["message"]["message_id"]
   chat_id = data["message"]["chat"]["id"]
   message = data["message"].get("text")
@@ -24,18 +24,18 @@ def webhook():
     requests.post(url,data={"chat_id":chat_id,"text":"Text!"})
   elif document:
     requests.post(url,data={"chat_id":chat_id,"text":"Document!"})
+    file_id = document = data["message"].get("document")["file_id"]
+    file_res = requests.get(f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}")
+    file_path = file_res["result"]["file_path"]
+    file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    requests.post(url,data={"chat_id":chat_id,"document":file_url})
   elif photo:
     requests.post(url,data={"chat_id":chat_id,"text":"Photo!"})
   elif audio:
     requests.post(url,data={"chat_id":chat_id,"text":"Audio!"})
   else:
     print(data)
-    requests.post(url,data={"chat_id":chat_id,"text":"Other!"})'''
-  '''
-  file_res = requests.get(f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}")
-  file_path = file_res["result"]["file_path"]
-  file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
-  '''
-  return 'testing'
+    requests.post(url,data={"chat_id":chat_id,"text":"Other!"})
+  return {"ok":True}
 if __name__ == "__main__":
   app.run(port=os.getenv("PORT", default=5000))
