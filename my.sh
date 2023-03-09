30692633855b552fc023fda84e3469ef7bb8c599
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-09 21:57:27 +0530

    Update my.sh

--- my.sh
+++ my.sh
@@ -1,16 +1,19 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake g++
+cd
+su -
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y make git zlib1g-dev libssl-dev gperf cmake g++
+exit
 git clone --recursive https://github.com/tdlib/telegram-bot-api.git
-cd telegram-bot-api
+cd telegram-bot-api/
 rm -rf build
 mkdir build
-cd build
+cd build/
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
 cmake --build . --target install
 cd ../..
 ls -l telegram-bot-api/bin/telegram-bot-api*
 
-cd telegram-bot-api
-cd bin
+cd telegram-bot-api/
+cd bin/
 ./telegram-bot-api --api-id=$TELEGRAM_API_ID --api-hash=$TELEGRAM_API_HASH
