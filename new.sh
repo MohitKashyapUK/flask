a44ab35adcbbb3d1a01346d4859bb3ea82687249
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-03-10 16:49:45 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,19 @@
+wget http://ftp.debian.org/debian/pool/main/m/make/make_4.3-4_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/g/git/git_2.30.2-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/z/zlib/zlib1g-dev_1.2.11.dfsg-2_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/o/openssl/libssl-dev_1.1.1k-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/g/gperf/gperf_3.1-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/c/cmake/cmake_3.18.4-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/l/llvm-toolchain-12/clang_12.0.1-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/libc++/libc++-dev_12.0.1-1_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/libc++abi/libc++abi-dev_12.0.1-1_amd64.deb
+
+sudo dpkg -i make_4.3-4_amd64.deb
+sudo dpkg -i git_2.30.2-1_amd64.deb
+sudo dpkg -i zlib1g-dev_1.2.11.dfsg-2_amd64.deb
+sudo dpkg -i libssl-dev_1.1.1k-1_amd64.deb
+sudo dpkg -i gperf_3.1-1_amd64.deb
+sudo dpkg -i cmake_3.18.4-1_amd64.deb
+sudo dpkg -i clang_12.0.1-1_amd64.deb
+sudo dpkg -i libc++-dev_12.0.1-1_amd64.deb
+sudo dpkg -i libc++abi-dev_12.0.1-1_amd64.deb
